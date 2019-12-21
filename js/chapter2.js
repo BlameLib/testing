cb8e3aad672124fd69ef0e39a940b764aec9886f
BlameLib <59034871+BlameLib@users.noreply.github.com>
2019-12-20 23:36:24 -0800

    Update chapter2.js

--- js/chapter2.js
+++ js/chapter2.js
@@ -10,17 +10,31 @@ script["Chapter-2"] =
     "show Kama Default with fadeIn"
     "Kama What's wrong with me? <br> Why am I still thinking about Chaldea's Master?",
     "Kama Regardless of what Parvati thinks...I am not going to take up that thankless job again.",
-    
+    "show Kama Smug",
     {"Conditional": {
       "Condition": function ()
       {
         return storage.player.sel == "Gudako";
       },
-      "True": "Kama Why, when Master of Chaldea's deadline ends, and she hasn't retrieved the presents, <br>I am just going to throw the remainder down this mountain.<br>That should show her. Hah!",
-      "False": "Kama Why, when Master of Chaldea's deadline ends, and he hasn't retrieved the presents, <br>I am just going to throw the remainder down this mountain.<br>That should show him. Hah!",
+      "True": "Kama Why, when Master of Chaldea's deadline ends, <br>and she hasn't retrieved the presents, 
+      "False": "Kama Why, when Master of Chaldea's deadline ends, <br>and he hasn't retrieved the presents, 
     }},
+    {"Conditional": {
+      "Condition": function ()
+      {
+        return storage.player.sel == "Gudako";
+      },
+      "True": "Kama I am just going to throw the remainder down this mountain.<br>That should show her. Hah!",
+      "False": "Kama I am just going to throw the remainder down this mountain.<br>That should show him. Hah!",
+    }},
+    
+    "play sound Walk-floor",
     "stop music", "play sound cabin-door-opening", "scene HotelNight with fadeIn",
     "show Kama Pissed", "scene HotelNight with flash", "scene HotelNight with flash", "play music SpinalCoaster", 
+    "Kama W-what? <br> Who are you and how did you get into my room?",
+    "show Pingu-Swag Sparkle",
+    "Pingu-Swag Well, well! Look who's here!",
+    
     "jump Chapter-2-End",
 ],
 
