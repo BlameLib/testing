985a66524faabee5afc596f85592dde1a9324a5a
BlameLib <59034871+BlameLib@users.noreply.github.com>
2019-12-30 18:42:42 -0800

    Update chapter2.js

--- js/chapter2.js
+++ js/chapter2.js
@@ -32,18 +32,29 @@ script["Chapter-2"] =
     "stop music", "play sound cabin-door-opening", "scene HotelNight with fadeIn",
     "show Kama Pissed", "scene HotelNight with flash", "scene HotelNight with flash", "play music SpinalCoaster", 
     "Kama W-what? <br> Who are you and how did you get into my room?",
-    "show Pingu-Swag Sparkle",
-    "Pingu-Swag Well, well! Look who's here!",
-    
+	
+    "show Pingu-Swag Sparkle with slideInRight", "show Kama Pissed left with grayscale",
+    "Pingu-Swag Well, well! Look who's here! <br> What hole did you crawl out from, Goddess of Love?",
+	"Pingu-Swag Only proper divas are allowed to be this fashionable late, you know?",
+	"show Pingu-Swag Smug",
+	"Pingu-Swag And you are not one.",
+	"show Pingu-Swag default right with grayscale", "show Kama Irritated left",
+	
+	"Kama I will not ask again. <br> Who. Are. You.",
+	"show Kama Irritated left with grayscale", "show Pingu-Swag Sparkle right",
+	
+	
+	"Pingu-Swag I am the Top Star, dontcha know?",
+	"Pingu-Swag The Dancing Queen. Young and Sweet! <br> Only Seventeen~",
+	"Pingu-Swag But YOU can call me Spirit of Christmas Present!",
+	"show Kama NotImpressed left", "show Pingu-Swag Smile right with grayscale",
+	
+	"Kama Really now. You?",
+	"Kama Since when does a self-proclaimed diva embody this festival of Giving?",
+    "show Kama NotImpressed left with grayscale", "show Pingu-Swag Smug right",
+	
     "jump Chapter-2-End",
 ],
-
-// script["Chapter-2-2"] =
-// [
-    
-    
-//    "jump Chapter-2-End",
-// ],
 
 script ["Chapter-2-End"]=
 [
